0e2de3d33dc55be5a8cd32d95b1dc147d09bcafa
vinay kumar <vinaycsc9@gmail.com>
2026-08-11 00:18:26 +0530

    fix: correct minimum length for name validation in registerValidation

--- backend/src/middlewares/validators.js
+++ backend/src/middlewares/validators.js
@@ -2,7 +2,7 @@ const { body, validationResult } = require('express-validator');
 
 const registerValidation = [
   body('name')
-    .isLength({ min: 20, max: 60 })
+    .isLength({ min: 5, max: 60 })
     .withMessage('Name must be between 20 and 60 characters.'),
   body('email')
     .isEmail()
